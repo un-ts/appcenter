668d103f95915b49de51cd961242c354dee4a7c6
JounQin <admin@1stg.me>
2023-03-28 16:01:35 +0000

    chore: use top level await

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,54 +2,49 @@ import fs from 'node:fs/promises'
 
 import { request } from '@octokit/request'
 
-const main = async () => {
-  /**
-   * @link https://github.com/sindresorhus/github-markdown-css
-   */
-  const { data } = await request('POST /markdown', {
-    text: await fs.readFile('README.md', 'utf8'),
-    token: process.env.GITHUB_TOKEN,
-  })
+/**
+ * @link https://github.com/sindresorhus/github-markdown-css
+ */
+const { data } = await request('POST /markdown', {
+  text: await fs.readFile('README.md', 'utf8'),
+  token: process.env.GITHUB_TOKEN,
+})
 
-  await fs.writeFile(
-    'public/index.html',
-    /* HTML */ `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>App Center</title>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css"
-          />
-          <style>
+await fs.writeFile(
+  'public/index.html',
+  /* HTML */ `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+        <title>App Center</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css"
+        />
+        <style>
+          .markdown-body {
+            box-sizing: border-box;
+            min-width: 200px;
+            max-width: 980px;
+            margin: 0 auto;
+            padding: 45px;
+          }
+
+          @media (max-width: 767px) {
             .markdown-body {
-              box-sizing: border-box;
-              min-width: 200px;
-              max-width: 980px;
-              margin: 0 auto;
-              padding: 45px;
+              padding: 15px;
             }
-
-            @media (max-width: 767px) {
-              .markdown-body {
-                padding: 15px;
-              }
-            }
-          </style>
-        </head>
-        <body>
-          <article class="markdown-body">${data}</article>
-        </body>
-      </html>
-    `,
-  )
-}
-
-// eslint-disable-next-line unicorn/prefer-top-level-await
-main().catch(console.error)
+          }
+        </style>
+      </head>
+      <body>
+        <article class="markdown-body">${data}</article>
+      </body>
+    </html>
+  `,
+)
